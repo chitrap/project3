a1aee15ffcd0e9586700dfe740a2bca16a34fcbd
kulkarnis <kulkarni.sau@husky.neu.edu>
2015-03-18 20:27:00 -0400

    Some more addition

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -19,6 +19,7 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "threads/malloc.h"
+#include "vm/frame.h"
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp,
@@ -495,14 +496,18 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
       /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
+      //-------
+      //uint8_t *kpage = palloc_get_page (PAL_USER);
+      uint8_t *kpage = get_frame (PAL_USER);
       if (kpage == NULL)
         return false;
 
       /* Load this page. */
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
-          palloc_free_page (kpage);
+          //----
+          //palloc_free_page (kpage);
+          free_vm_frames (kpage);
           return false; 
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
@@ -510,7 +515,9 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable)) 
         {
-          palloc_free_page (kpage);
+          //---------
+          //palloc_free_page (kpage);
+          free_vm_frames (kpage);
           return false; 
         }
 
@@ -532,14 +539,20 @@ setup_stack (void **esp, const char *file_name, char **save_ptr)
   uint8_t *kpage;
   bool success = false;
 
-  kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+//--------
+  //kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+  kpage = get_frame (PAL_USER | PAL_ZERO);
   if (kpage != NULL) 
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
         *esp = PHYS_BASE;
       else
-        palloc_free_page (kpage);
+      {
+        //-----------
+        //palloc_free_page (kpage);
+        free_vm_frames (kpage);
+      }
     }
   
   const void *user_stack_bottom = *esp - PGSIZE;
--- src/vm/frame.c
+++ src/vm/frame.c
@@ -9,6 +9,8 @@ static struct hash frames;
 
 unsigned frame_hash (const struct hash_elem *, void *);
 bool frame_comparator (const struct hash_elem *, const struct hash_elem *, void *);
+static bool delete_frame (void *);
+static struct struct_frame *find_frame (void *);
 
 //Initializes frames hash, called from threads/init.c
 //from main function
@@ -40,4 +42,108 @@ frame_comparator (const struct hash_elem *first_, const struct hash_elem *second
 	const struct struct_frame *s = hash_entry (second_, 
 		                           struct struct_frame, hash_elem); 
 	return f->page < s->page;
+}
+
+
+//Frees resources
+void
+free_vm_frames (void *pg)
+{
+	delete_frame (pg);
+	palloc_free_page (pg);
+}
+
+//Deletes a frame, free its memory
+static bool
+delete_frame (void *pg)
+{
+	struct struct_frame *f = find_frame (pg);
+	if (f == NULL)
+	 {
+	 	return false;
+	 }
+
+	 lock_acquire (&frame_lock);
+	 hash_delete (&frames, &f->hash_elem);
+	 free (f);
+	 lock_release (&frame_lock);
+
+	 return true;
+}
+
+
+//returns frame for given page
+static struct struct_frame *
+find_frame (void *pg)
+{
+	struct struct_frame sf;
+	struct hash_elem *elm;
+
+	sf.page = pg;
+	elm = hash_find (&frames, &sf.hash_elem);
+	if (elm == NULL)
+	 {
+	 	return NULL;
+	 }
+
+	return hash_entry (elm, struct struct_frame, hash_elem); 
+
+}
+
+//Maps User virtual page to frame of vm by help of pte
+bool
+upage_to_frame_mapping (void *frame, uint32_t *pte, void *vaddr)
+{
+	struct struct_frame *sf = find_frame (frame);
+	if (sf == NULL)
+	 {
+	 	return false;
+	 }
+	 sf->pte = pte;
+	 sf->vaddr = vaddr;
+
+	 return true;
+}
+
+//Adds frame to Hash by allocating memory
+static bool
+add_frame (void *pg)
+{
+	struct struct_frame *sf;
+	sf = (struct struct_frame *) malloc (sizeof (struct struct_frame));
+
+	if(sf == NULL)
+	 {
+	 	return false;
+	 }
+
+	 sf->thread = thread_current ();
+	 sf->page = pg;
+
+	 lock_acquire (&frame_lock);
+	 hash_insert (&frames, &sf->hash_elem);
+	 lock_release (&frame_lock);
+
+	 return true;
+}
+
+//Gets a free frame
+void *
+get_frame (enum palloc_flags flags)
+{
+	void *pg = palloc_get_page (flags);
+	if (pg != NULL)
+	 {
+	 	add_frame (pg);
+	 	find_frame (pg);
+	 }
+	 else
+	  {
+	  	#ifndef vm
+	  		sys_exit (-1);
+	  	#endif
+	  	PANIC ("Eviction needed !");	
+	  }
+
+	  return pg;
 }
